f2fb81df8e998ce8c2f8d8d8faae1ae9bdc99682
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2022-11-17 17:34:44 +0000

    Add lambda to get the upload data.
    This lambda is at the start of the step function process for the new
    backend checks. It:
    
    * Gets the file IDs and original paths from the API
    * Gets the files from the API
    * Compares them to make sure they match.
    * Returns fileId, consignmentId and original path for all files in the
      consignment.
    
    This is then used by the other backend check lambdas.

---
+++ src/lambda_handler.py
@@ -0,0 +1,102 @@
+from sgqlc.endpoint.http import HTTPEndpoint
+from sgqlc.operation import Operation
+from sgqlc.types.relay import Connection
+from sgqlc.types import Type, Field, list_of
+from boto3 import client
+import os
+import requests
+
+
+class FileMetadata(Type):
+    name = Field(str)
+    value = Field(str)
+
+
+class File(Type):
+    fileId = Field(str)
+    fileType = Field(str)
+    fileMetadata = list_of(FileMetadata)
+
+
+class Consignment(Connection):
+    files = list_of(File)
+
+
+class Query(Type):
+    getConsignment = Field(Consignment, args={'consignmentid': str})
+
+
+def get_client_secret():
+    client_secret_path = os.environ["CLIENT_SECRET_PATH"]
+    ssm_client = client("ssm")
+    response = ssm_client.get_parameter(
+        Name=client_secret_path,
+        WithDecryption=True
+    )
+    return response["Parameter"]["Value"]
+
+
+def get_token(client_secret):
+    client_id = os.environ["CLIENT_ID"]
+    auth_url = f'{os.environ["AUTH_URL"]}/realms/tdr/protocol/openid-connect/token'
+    grant_type = {"grant_type": "client_credentials"}
+    auth_response = requests.post(auth_url, data=grant_type, auth=(client_id, client_secret))
+    if auth_response.status_code != 200:
+        raise RuntimeError(f"Non 200 status from Keycloak {auth_response.status_code}")
+    return auth_response.json()['access_token']
+
+
+def get_query(consignment_id):
+    operation = Operation(Query)
+    consignment = operation.getConsignment(consignmentid=consignment_id)
+    files = consignment.files()
+    files.fileId()
+    files.fileType()
+    files.fileMetadata()
+    return operation
+
+
+def process_file(file: File):
+    file_path = [data['value'] for data in file.fileMetadata if data.name == "ClientSideOriginalFilepath"][0]
+    return {'fileId': file.fileId, 'originalPath': file_path}
+
+
+def s3_list_files(prefix):
+    s3_client = client("s3")
+    truncated = True
+    marker = ''
+    response = None
+    while truncated:
+        response = s3_client.list_objects(
+            Bucket=os.environ['BUCKET_NAME'],
+            Prefix=prefix,
+            Marker=marker
+        )
+        truncated = response["IsTruncated"]
+        marker = response['Marker'] if truncated else ''
+    return [entry["Key"].split("/")[2] for entry in response["Contents"]]
+
+
+def validate_all_files_uploaded(prefix, consignment: Consignment):
+    api_files = [file.fileId for file in consignment.files if file.fileType == "File"]
+    s3_files = s3_list_files(prefix)
+    api_files.sort()
+    s3_files.sort()
+    if api_files != s3_files:
+        raise RuntimeError(f"Uploaded files do not match files from the API for {prefix}")
+
+
+def handler(event, lambda_context):
+    user_id = event["userId"]
+    consignment_id = event["consignmentId"]
+    query = get_query(consignment_id)
+    client_secret = get_client_secret()
+    api_url = os.environ["API_URL"]
+    headers = {'Authorization': f'Bearer {get_token(client_secret)}'}
+    endpoint = HTTPEndpoint(api_url, headers, 300)
+    data = endpoint(query)
+    if 'errors' in data:
+        raise Exception("Error in response", data['errors'])
+    consignment = (query + data).getConsignment
+    validate_all_files_uploaded(f"{user_id}/{consignment_id}", consignment)
+    return [process_file(file) | {'userId': user_id} for file in consignment.files]
---
+++ tests/test_lambda.py
@@ -0,0 +1,90 @@
+import urllib
+from unittest.mock import patch
+import pytest
+from moto import mock_ssm, mock_s3
+import boto3
+from src import lambda_handler
+from tests.utils.utils import *
+
+
+@pytest.fixture(scope='function')
+def ssm():
+    with mock_ssm():
+        yield boto3.client('ssm', region_name='eu-west-2')
+
+
+@pytest.fixture(scope='function')
+def s3():
+    with mock_s3():
+        yield boto3.client('s3', region_name='eu-west-2')
+
+
+@patch('urllib.request.urlopen')
+def test_files_are_returned(mock_url_open, ssm, s3):
+    setup_env_vars()
+    setup_ssm(ssm)
+    setup_s3(s3)
+    configure_mock_urlopen(mock_url_open, graphql_ok_multiple_files)
+    event = {'userId': user_id, 'consignmentId': consignment_id}
+    with patch('src.lambda_handler.requests.post') as mock_post:
+        mock_post.return_value.status_code = 200
+        mock_post.return_value.json = access_token
+        response = lambda_handler.handler(event, None)
+        response.sort(key=sort_by_id)
+        file_one = response[0]
+        file_two = response[1]
+        assert file_one["fileId"] == "13702546-da63-4545-a9eb-a892df1aafba"
+        assert file_one["originalPath"] == "testfile/subfolder/subfolder2.txt"
+        assert file_one["userId"] == user_id
+        assert file_two["fileId"] == "1c2b9eeb-2e4c-4cfc-bc08-c193660f86d2"
+        assert file_two["originalPath"] == "testfile/subfolder/subfolder1.txt"
+        assert file_two["userId"] == user_id
+
+
+@patch('urllib.request.urlopen')
+def test_error_from_graphql_api(mock_url_open, ssm, s3):
+    setup_env_vars()
+    setup_ssm(ssm)
+    setup_s3(s3)
+    err = urllib.error.HTTPError(
+        'http://testserver.com',
+        500,
+        'Some Error',
+        {'Xpto': 'abc'},
+        io.BytesIO(b'xpto'),
+    )
+    event = {'userId': user_id, 'consignmentId': consignment_id}
+    configure_mock_urlopen(mock_url_open, err)
+    with patch('src.lambda_handler.requests.post') as mock_post:
+        mock_post.return_value.status_code = 200
+        mock_post.return_value.json = access_token
+        with pytest.raises(Exception) as ex:
+            lambda_handler.handler(event, None)
+        assert ex.value.args[1][0]['message'] == 'HTTP Error 500: Some Error'
+
+
+def test_error_from_keycloak(ssm, s3):
+    setup_env_vars()
+    setup_ssm(ssm)
+    setup_s3(s3)
+    event = {'userId': user_id, 'consignmentId': consignment_id}
+    with patch('src.lambda_handler.requests.post') as mock_post:
+        mock_post.return_value.status_code = 500
+        with pytest.raises(RuntimeError) as ex:
+            lambda_handler.handler(event, None)
+        assert ex.value.args[0] == 'Non 200 status from Keycloak 500'
+
+
+@patch('urllib.request.urlopen')
+def test_error_if_s3_files_mismatch(mock_url_open, ssm, s3):
+    setup_env_vars()
+    setup_ssm(ssm)
+    setup_s3(s3, missing_file_id)
+    event = {'userId': user_id, 'consignmentId': consignment_id}
+    configure_mock_urlopen(mock_url_open, graphql_ok_multiple_files)
+    with patch('src.lambda_handler.requests.post') as mock_post:
+        mock_post.return_value.status_code = 200
+        mock_post.return_value.json = access_token
+        with pytest.raises(RuntimeError) as ex:
+            lambda_handler.handler(event, None)
+        assert ex.value.args[0] == f'Uploaded files do not match files from the API for {user_id}/{consignment_id}'
